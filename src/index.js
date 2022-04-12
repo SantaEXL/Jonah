4d9d358f28e471cec6a87a510afef187df6d7463
Syprene <TRMayhem@outlook.com>
2022-04-12 10:53:46 -0700

    add: initialCommit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+import './resources/theming.scss'
+
+import { Button } from 'react-bootstrap';
+import PageLayout from './resources/layouts/PageLayout';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <PageLayout>
+        <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+        <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin></script>
+        <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>
+        <script>var Alert = ReactBootstrap.Alert;</script>
+    </PageLayout>,
+    document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
